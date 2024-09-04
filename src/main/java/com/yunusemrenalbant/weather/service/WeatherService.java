e1f79d4c8c7a71cc1354bbe2af96cc1a9e620cd3
Yunus Emre Nalbant <yunus.nalbant0@gmail.com>
2024-09-04 13:54:14 +0300

    caching

--- src/main/java/com/yunusemrenalbant/weather/service/WeatherService.java
+++ src/main/java/com/yunusemrenalbant/weather/service/WeatherService.java
@@ -4,6 +4,14 @@ import com.yunusemrenalbant.weather.client.WeatherClient;
 import com.yunusemrenalbant.weather.dto.weather.WeatherDto;
 import com.yunusemrenalbant.weather.model.Weather;
 import com.yunusemrenalbant.weather.repository.WeatherRepository;
+import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
@@ -11,7 +19,10 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 @Service
+@CacheConfig(cacheNames = {"weathers"})
 public class WeatherService {
+
+    private static final Logger logger = LoggerFactory.getLogger(WeatherService.class);
 
     private final WeatherRepository weatherRepository;
     private final WeatherClient weatherClient;
@@ -24,25 +35,29 @@ public class WeatherService {
         this.weatherClient = weatherClient;
     }
 
+    @Cacheable(key = "#cityName")
     public Mono<WeatherDto> getWeatherByCityName(String cityName) {
         return weatherRepository.findFirstByRequestedCityNameOrderByUpdatedTimeDesc(cityName)
                 .flatMap(weatherDto -> {
-
-                    if (weatherDto.updatedTime().isBefore(LocalDateTime.now().minusSeconds(30))) {
+                    if (weatherDto != null && weatherDto.updatedTime().isBefore(LocalDateTime.now().minusMinutes(60))) {
                         return getWeatherFromWeatherStack(cityName);
                     }
-                    return Mono.just(weatherDto);
+                    logger.info("Getting weather from database for {} due to it is already up-to-date", cityName);
+
+                    return Mono.justOrEmpty(weatherDto);
                 })
                 .switchIfEmpty(getWeatherFromWeatherStack(cityName))
-                .onErrorResume(e -> Mono.error(new RuntimeException("Error processing weather data", e)));
+                .onErrorResume(e -> Mono.error(new RuntimeException(e.getMessage())));
     }
 
+    @CachePut(key = "#cityName")
     public Mono<WeatherDto> getWeatherFromWeatherStack(String cityName) {
+        logger.info("Requesting weather stack api for city: {}", cityName);
+
         return weatherClient.getWeatherByCityName(cityName)
                 .flatMap(response -> {
                     try {
                         DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
-
                         Weather weather = new Weather(
                                 cityName,
                                 response.location().name(),
@@ -60,4 +75,10 @@ public class WeatherService {
                 });
     }
 
+    @CacheEvict(allEntries = true)
+    @PostConstruct
+    @Scheduled(fixedRateString = "${weather-stack.cache-ttl}")
+    public void clearCache(){
+        logger.info("Caches are cleared");
+    }
 }
